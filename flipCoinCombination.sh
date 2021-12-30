7980efae4b3773fc71dde62a6caaf82f188d63c3
srk-333 <sauravmk333@gmail.com>
2021-12-30 22:45:43 +0530

    Added: Percentage evalution o Doublet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -3,36 +3,50 @@
 echo "Welcome to Flip Coin Combination Problem"
 
 Head=1;
-Limit=150;
+Limit=100;
 
-hCount=0;
-tCount=0;
-totalCount=0;
+hh=0;
+ht=0;
+th=0;
+tt=0;
+flag=0;
 
 declare -A result
 
-while [ $totalCount -lt $Limit ]
+while [ $flag -lt $Limit ]
  do
-         coinFlip=$(( RANDOM%2 ));
+         flip1=$(( RANDOM%2 ));
+         flip2=$(( RANDOM%2 ));
 
-        if [[ $coinFlip -eq $Head ]]
+        if [ $flip1 -eq 1 ] && [ $flip2 -eq 1 ]
            then
-                   let hCount++
-           else
-                  let tCount++
+                   let hh++
+           elif [ $flip1 -eq 1 ] && [ $flip2 -eq 0 ]
+           then
+                  let ht++
+           elif [ $flip1 -eq 0 ] && [ $flip2 -eq 1 ]
+           then
+                  let th++
+           else 
+                  let tt++
        fi
-       let totalCount++
+       let flag++
  done
 
-   headPercentage=`echo $hCount $Limit | awk '{h=$1/$2*100} {print h}'`
+   hhPercentage=`echo $hh $Limit | awk '{hh=$1/$2*100} {print hh}'`
+   htPercentage=`echo $ht $Limit | awk '{ht=$1/$2*100} {print ht}'`
+   thPercentage=`echo $th $Limit | awk '{th=$1/$2*100} {print th}'`
+   ttPercentage=`echo $tt $Limit | awk '{tt=$1/$2*100} {print tt}'`
 
-   tailPercentage=`echo $tCount $Limit | awk '{t=$1/$2*100} {print t}'`
-
-  result[h]=#headPercentage
-  result[t]=$tailPercentage
+  result[hh]=$hhPercentage
+  result[ht]=$htPercentage
+  result[th]=$thPercentage
+  result[tt]=$ttPercentage
 
    echo ${result[@]}
 
-echo "Head percentage $headPercentage%"
-echo "Tail percentage $tailPercentage%"
+echo "Combination of HH is $hhPercentage%"
+echo "Combination of HT is  $htPercentage%"
+echo "Combination of TH is $thPercentage%"
+echo "Combination of TT is  $ttPercentage%"
    
