edf7db88cf0d97e173296ec185fc750484c36e97
srk-333 <sauravmk333@gmail.com>
2021-12-30 10:15:11 +0530

    Added: to flip coin

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 
 echo "Welcome to Flip Coin Combination Problem"
+
+coinFlip=$(( RANDOM%2 ));
+Head=1;
+
+ if [[ $coinFlip -eq $Head ]]
+    then
+        echo "It's Head"
+    else
+        echo "It's Tail"
+ fi
