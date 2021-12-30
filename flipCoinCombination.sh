6f78a1ab217e25c62a10c36cc463aa7859268bcd
srk-333 <sauravmk333@gmail.com>
2021-12-30 23:44:46 +0530

    Added: modification for triplet doublet singlet

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,8 +2,9 @@
 
 echo "Welcome to Flip Coin Combination Problem"
 
-Head=1;
 Limit=100;
+
+###Triplet Coin Flip
 
 hhh=0;
 hht=0;
@@ -11,11 +12,11 @@ hth=0;
 ttt=0;
 tth=0;
 tht=0;
-flag=0;
+tp=0;
 
-declare -A result
+declare -A triplet
 
-while [ $flag -lt $Limit ]
+while [ $tp -lt $Limit ]
  do
          flip1=$(( RANDOM%2 ));
          flip2=$(( RANDOM%2 ));
@@ -44,7 +45,7 @@ while [ $flag -lt $Limit ]
            else 
                   let tht++
        fi
-       let flag++
+       let tp++
  done
 
    hhhPercentage=`echo $hhh $Limit | awk '{hhh=$1/$2*100} {print hhh}'`
@@ -54,18 +55,85 @@ while [ $flag -lt $Limit ]
    tthPercentage=`echo $tth $Limit | awk '{tth=$1/$2*100} {print tth}'`
    thtPercentage=`echo $tht $Limit | awk '{tht=$1/$2*100} {print tht}'`
 
-  result[hhh]=$hhhPercentage
-  result[hht]=$hhtPercentage
-  result[hth]=$hthPercentage
-  result[ttt]=$tttPercentage
-  result[tth]=$tthPercentage
-  result[tht]=$thtPercentage
+  triplet[hhh]=$hhhPercentage
+  triplet[hht]=$hhtPercentage
+  triplet[hth]=$hthPercentage
+  triplet[ttt]=$tttPercentage
+  triplet[tth]=$tthPercentage
+  triplet[tht]=$thtPercentage
 
-   echo ${result[@]}
+   echo ${triplet[@]}
 
-echo "Combination of HHH is $hhhPercentage%"
-echo "Combination of HHT is  $hhtPercentage%"
-echo "Combination of HTH is $hthPercentage%"
-echo "Combination of TTT is  $tttPercentage%"
-echo "Combination of TTH is  $tthPercentage%"
-echo "Combination of THT is  $thtPercentage%"   
+###Doublet Coin Flip
+
+hh=0;
+ht=0;
+th=0;
+tt=0;
+db=0;
+
+declare -a doublet
+
+while [ $db -lt $Limit ]
+ do
+         flip1=$(( RANDOM%2 ));
+         flip2=$(( RANDOM%2 ));
+
+      if [ $flip1 -eq 1 ] && [ $flip2 -eq 1 ]
+         then
+               let hh++
+         elif [ $flip1 -eq 1 ] && [ $flip2 -eq 0 ]
+         then
+               let ht++
+         elif [ $flip1 -eq 0 ] && [ $flip2 -eq 1 ]
+         then
+               let th++
+        else 
+              let tt++
+      fi
+     let db++
+done
+
+   hhPercentage=`echo $hh $Limit | awk '{hh=$1/$2*100} {print hh}'`
+   htPercentage=`echo $ht $Limit | awk '{ht=$1/$2*100} {print ht}'`
+   thPercentage=`echo $th $Limit | awk '{th=$1/$2*100} {print th}'`
+   ttPercentage=`echo $tt $Limit | awk '{tt=$1/$2*100} {print tt}'`
+
+  doublet[hh]=$hhPercentage
+  doublet[ht]=$htPercentage
+  doublet[th]=$thPercentage
+  doublet[tt]=$ttPercentage
+
+   echo ${doublet[@]}
+
+### Singlet Coin Flip
+
+head=1;
+tail=0;
+
+hCount=0;
+tCount=0;
+sg=0;
+
+declare -A singlet
+
+while [ $sg -lt $Limit ]
+  do
+         flip1=$(( RANDOM%2 ));
+
+        if [ $flip1 -eq $head ]
+           then
+              let  hCount++
+           else
+              let tCount++
+        fi
+         let sg++
+ done
+
+   headPercentage=`echo $hCount $Limit | awk '{h=$1/$2*100} {print h}'`
+   tailPercentage=`echo $tCount $Limit | awk '{t=$1/$2*100} {print t}'`
+
+  singlet[h]=$headPercentage
+  singlet[t]=$tailPercentage
+
+   echo ${singlet[@]}
